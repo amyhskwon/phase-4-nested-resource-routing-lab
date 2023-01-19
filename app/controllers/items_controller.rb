9a3c80da6a8128bede7044865d075eefee9fff74
amyhskwon <amyhskwon@gmail.com>
2023-01-19 00:47:12 -0800

    commit tree 76651463d1a4e58cb22bbe4adad270c889aeacb5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_message
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
 
   def index
-    items = Item.all
+    user_id = (params[:user_id])
+    items = user_id ? User.find(user_id).items : Item.all
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_message
+    render json: {error: "User ID not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_record_invalid(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
 end
